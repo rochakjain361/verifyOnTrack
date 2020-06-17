4d45909088a20fb562730828e981accc987036d0
manjunathvustepalli <manjunathvustepalli@users.noreply.github.com>
2020-06-17 11:56:51 +0530

    state and lga

--- src/Components/AdminPageComponents/Settings/AddressSettings/ManageStates/index.js
+++ src/Components/AdminPageComponents/Settings/AddressSettings/ManageStates/index.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { CircularProgress } from "@material-ui/core";
 
 import IconButton from "@material-ui/core/IconButton";
 import PhoneIcon from '@material-ui/icons/Phone';
@@ -15,204 +16,173 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import axios from "axios";
 
-const rows = [
-    {
-        "state": "testState1",
-    },
-    {
-        "state": "testState2"
-    },
-    {
-        "state": "testState3",
-    },
-    {
-        "state": "testState4"
-    }
-];
+let result=[];
+
 
 const styles = theme => ({
 
 })
 
+const token1 = localStorage.getItem("Token");
+const token = "Token " + token1;
+const id = localStorage.getItem("id");
+ 
+
+//   const { classes } = this.props;
 export class index extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+          loading: true,
+          enteredtext:"",
+          selectedstate:""
+        };
+    } isloading() {
+    return (
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        display="flex"
+        style={{ minHeight: "100vh" }}
+      >
+        <CircularProgress />
+      </Grid>
+    );
+  }
+    
+    async componentDidMount(){
+        await axios
+          .get(
+            "http://3.22.17.212:8000/api/v1/resManager/address/states/" ,
+            {
+              headers: {
+                Authorization: token,
+              },
+            }
+          )
+          .then((res) => {
+            result = res.data;
+            console.log(result);
+          });
+          this.setState({loading:false})
+    }
+    displaytable(){
+        return (
+          <>
+            <Grid
+              container
+              justify="space-between"
+              alignItems="center"
+              spacing={4}
+            >
+              <Grid item>
+                <Typography variant="h4">States</Typography>
+              </Grid>
+
+              <Grid item xs={6}>
+                <Autocomplete
+                  options={result}
+                  getOptionLabel={(option) => option.stateName}
+                  size="small"
+                  id="states"
+                  Username
+                  value={this.state.selectedstate}
+                  onChange={(event, value) => {
+                    this.setState({ selectedstate: value });
+                    console.log("selectedstate", value);
+                 
+                  }}
+                  inputValue={this.state.enteredtext}
+                  onInputChange={(event, newInputValue) => {
+                    this.setState({ enteredtext: newInputValue });
+                    // console.log(newInputValue);
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="States"
+                      margin="normal"
+                      variant="outlined"
+                      size="small"
+                    />
+                  )}
+                />
+              </Grid>
+            </Grid>
+
+            <Grid
+              container
+              justify="flex-start"
+              alignItems="center"
+              style={{ marginTop: 20 }}
+              spacing={2}
+            >
+              <Grid item xs={3}>
+                <TextField
+                  label="Enter new state"
+                  variant="outlined"
+                  size="medium"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item>
+                <Fab size="small" color="secondary">
+                  <AddIcon />
+                </Fab>
+              </Grid>
+
+              <TableContainer
+                component={Paper}
+                style={{ marginTop: 20, marginLeft: 10, marginRight: 10 }}
+                elevation={5}
+              >
+                <Table stickyHeader>
+                  <TableHead>
+                    <TableRow style={{ backgroundColor: "black" }}>
+                      <TableCell align="left">State</TableCell>
+                      <TableCell align="right"></TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {result.map((row, index) => (
+                      <TableRow key={row.id}>
+                        <TableCell align="left">{row.stateName}</TableCell>
+                        <TableCell align="right">
+                          <Button color="primary" variant="outlined">
+                            Delete
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+          </>
+        );
+    }
     render() {
 
-        const defaultProps = {
-            options: top100Films,
-            getOptionLabel: (option) => option.title,
-        };
-
-        const { classes } = this.props;
+      
 
         return (
-            <div style={{ marginTop: 20 }}>
-                {/* <Paper style={{ padding: 20, height: '100vh' }}> */}
-                <Grid container justify='space-between' alignItems='center' spacing={4}>
 
-                    <Grid item>
-                        <Typography variant='h4'>
-                            States
-                            </Typography>
-                    </Grid>
-
-                    <Grid item xs={6}>
-                        <Autocomplete
-                            size='small'
-                            {...defaultProps}
-                            id="states"
-                            Username
-                            renderInput={(params) => <TextField {...params} label="States" margin="normal" variant='outlined' size='small' />}
-                        />
-                    </Grid>
-
-                </Grid>
-
-                <Grid container justify='flex-start' alignItems='center' style={{ marginTop: 20 }} spacing={2}>
-
-                    <Grid item xs={3}>
-
-                        <TextField
-                            label="Enter new state to add"
-                            variant='outlined'
-                            size='medium'
-                            fullWidth
-                        />
-                    </Grid>
-                    <Grid item>
-                        <Fab size="small" color="secondary">
-                            <AddIcon />
-                        </Fab>
-                    </Grid>
-
-                    <TableContainer component={Paper} style={{ marginTop: 20, marginLeft: 10, marginRight: 10 }} elevation={5}>
-                        <Table stickyHeader>
-                            <TableHead>
-                                <TableRow style={{ backgroundColor: 'black' }}>
-                                    <TableCell align="left">State</TableCell>
-                                    <TableCell align="right"></TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {rows.map((row, index) => (
-                                    <TableRow key={row.id}>
-                                        <TableCell align="left">{row.state}</TableCell>
-                                        <TableCell align="right"><Button color="primary" variant="outlined">Delete</Button></TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-
-                </Grid>
-                {/* </Paper> */}
-            </div>
-        )
+          <div style={{ marginTop: 20 }}>
+            {/* <Paper style={{ padding: 20, height: '100vh' }}> */}
+            {this.state.loading?this.isloading():this.displaytable()}
+           
+            {/* </Paper> */}
+          </div>
+        );
     }
 }
 
-const top100Films = [
-    { title: 'The Shawshank Redemption', year: 1994 },
-    { title: 'The Godfather', year: 1972 },
-    { title: 'The Godfather: Part II', year: 1974 },
-    { title: 'The Dark Knight', year: 2008 },
-    { title: '12 Angry Men', year: 1957 },
-    { title: "Schindler's List", year: 1993 },
-    { title: 'Pulp Fiction', year: 1994 },
-    { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
-    { title: 'The Good, the Bad and the Ugly', year: 1966 },
-    { title: 'Fight Club', year: 1999 },
-    { title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
-    { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
-    { title: 'Forrest Gump', year: 1994 },
-    { title: 'Inception', year: 2010 },
-    { title: 'The Lord of the Rings: The Two Towers', year: 2002 },
-    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-    { title: 'Goodfellas', year: 1990 },
-    { title: 'The Matrix', year: 1999 },
-    { title: 'Seven Samurai', year: 1954 },
-    { title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
-    { title: 'City of God', year: 2002 },
-    { title: 'Se7en', year: 1995 },
-    { title: 'The Silence of the Lambs', year: 1991 },
-    { title: "It's a Wonderful Life", year: 1946 },
-    { title: 'Life Is Beautiful', year: 1997 },
-    { title: 'The Usual Suspects', year: 1995 },
-    { title: 'Léon: The Professional', year: 1994 },
-    { title: 'Spirited Away', year: 2001 },
-    { title: 'Saving Private Ryan', year: 1998 },
-    { title: 'Once Upon a Time in the West', year: 1968 },
-    { title: 'American History X', year: 1998 },
-    { title: 'Interstellar', year: 2014 },
-    { title: 'Casablanca', year: 1942 },
-    { title: 'City Lights', year: 1931 },
-    { title: 'Psycho', year: 1960 },
-    { title: 'The Green Mile', year: 1999 },
-    { title: 'The Intouchables', year: 2011 },
-    { title: 'Modern Times', year: 1936 },
-    { title: 'Raiders of the Lost Ark', year: 1981 },
-    { title: 'Rear Window', year: 1954 },
-    { title: 'The Pianist', year: 2002 },
-    { title: 'The Departed', year: 2006 },
-    { title: 'Terminator 2: Judgment Day', year: 1991 },
-    { title: 'Back to the Future', year: 1985 },
-    { title: 'Whiplash', year: 2014 },
-    { title: 'Gladiator', year: 2000 },
-    { title: 'Memento', year: 2000 },
-    { title: 'The Prestige', year: 2006 },
-    { title: 'The Lion King', year: 1994 },
-    { title: 'Apocalypse Now', year: 1979 },
-    { title: 'Alien', year: 1979 },
-    { title: 'Sunset Boulevard', year: 1950 },
-    { title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', year: 1964 },
-    { title: 'The Great Dictator', year: 1940 },
-    { title: 'Cinema Paradiso', year: 1988 },
-    { title: 'The Lives of Others', year: 2006 },
-    { title: 'Grave of the Fireflies', year: 1988 },
-    { title: 'Paths of Glory', year: 1957 },
-    { title: 'Django Unchained', year: 2012 },
-    { title: 'The Shining', year: 1980 },
-    { title: 'WALL·E', year: 2008 },
-    { title: 'American Beauty', year: 1999 },
-    { title: 'The Dark Knight Rises', year: 2012 },
-    { title: 'Princess Mononoke', year: 1997 },
-    { title: 'Aliens', year: 1986 },
-    { title: 'Oldboy', year: 2003 },
-    { title: 'Once Upon a Time in America', year: 1984 },
-    { title: 'Witness for the Prosecution', year: 1957 },
-    { title: 'Das Boot', year: 1981 },
-    { title: 'Citizen Kane', year: 1941 },
-    { title: 'North by Northwest', year: 1959 },
-    { title: 'Vertigo', year: 1958 },
-    { title: 'Star Wars: Episode VI - Return of the Jedi', year: 1983 },
-    { title: 'Reservoir Dogs', year: 1992 },
-    { title: 'Braveheart', year: 1995 },
-    { title: 'M', year: 1931 },
-    { title: 'Requiem for a Dream', year: 2000 },
-    { title: 'Amélie', year: 2001 },
-    { title: 'A Clockwork Orange', year: 1971 },
-    { title: 'Like Stars on Earth', year: 2007 },
-    { title: 'Taxi Driver', year: 1976 },
-    { title: 'Lawrence of Arabia', year: 1962 },
-    { title: 'Double Indemnity', year: 1944 },
-    { title: 'Eternal Sunshine of the Spotless Mind', year: 2004 },
-    { title: 'Amadeus', year: 1984 },
-    { title: 'To Kill a Mockingbird', year: 1962 },
-    { title: 'Toy Story 3', year: 2010 },
-    { title: 'Logan', year: 2017 },
-    { title: 'Full Metal Jacket', year: 1987 },
-    { title: 'Dangal', year: 2016 },
-    { title: 'The Sting', year: 1973 },
-    { title: '2001: A Space Odyssey', year: 1968 },
-    { title: "Singin' in the Rain", year: 1952 },
-    { title: 'Toy Story', year: 1995 },
-    { title: 'Bicycle Thieves', year: 1948 },
-    { title: 'The Kid', year: 1921 },
-    { title: 'Inglourious Basterds', year: 2009 },
-    { title: 'Snatch', year: 2000 },
-    { title: '3 Idiots', year: 2009 },
-    { title: 'Monty Python and the Holy Grail', year: 1975 },
-];
 
 export default withStyles(styles)(index);
 
