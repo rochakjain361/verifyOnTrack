2bb5e08ec92b165cfee6eaa801654e31a0ff16d9
manjunathvustepalli <manjunathvustepalli@gmail.com>
2020-07-13 23:13:58 +0530

    m=academics get update add completed

--- src/API.js
+++ src/API.js
@@ -14,27 +14,64 @@ let res=[]
 const token = localStorage.getItem("Token");
 export const get=async(url, params)=>{
     // const { classes } = this.props;
-     await Axios.get(url, {
+     return await Axios.get(url, {
       headers: {
         Authorization: token,
       },
-    }).then((response) => {
-       res=response;
-        ShowSuccessSnackbar("added succesfully")
-     
+    }).then(response => {
+       let res=response;
+        // ShowSuccessSnackbar("added succesfully")
+        return res;
     }).catch((error)=>{
-       res=error
-        console.log(error.response.status)
-        if(error.response.status===401){
+      console.log("///////////////////////////");
+        console.log(error)
+        // if(error.response.status===401){
             
-        }
-
-
-    })
-    return res;
-    console.log("res",res)
-    
+        // }
+    }) 
   }
+  export const update=async(url,formdata)=>{
+    let headers = {
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+    };
+   return await Axios.post(url,formdata,headers ).then((response) => {
+       res=response;
+        ShowSuccessSnackbar("updated succesfully")
+       return res
+    }).catch((error)=>{
+     
+       res=error
+        // console.log(error.response.status)
+        // if(error.response.status===401){
+            
+        // }
+  })
+}
+export const post=async(url,formdata)=>{
+  let headers = {
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+    },
+  };
+ return await Axios.post(url,formdata,headers ).then((response) => {
+     res=response;
+      ShowSuccessSnackbar("Added succesfully")
+     return res
+  }).catch((error)=>{
+   
+     res=error
+      // console.log(error.response.status)
+      // if(error.response.status===401){
+          
+      // }
+})
+
+}
+
   
 
 
