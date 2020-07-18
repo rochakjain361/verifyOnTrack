e51c87bc59ef5b6d6317401873f92630d10ff39c
manjunathvustepalli <manjunathvustepalli@gmail.com>
2020-07-18 11:08:02 +0530

    employment comments completed

--- src/Components/DashBoardComponents/MyCodes/EmployementCodes/Comments.js
+++ src/Components/DashBoardComponents/MyCodes/EmployementCodes/Comments.js
@@ -38,19 +38,43 @@ import ListSubheader from '@material-ui/core/ListItemText';
 function Comments(props) {
   return (
     <div>
-        <List>
-      {props.data.map((comment) => (<Grid  justify="flex-start">
-
-       
-<h3>
-{comment.company_name_field}
-</h3>
-<ListItemText>
-{comment.comment}
-</ListItemText>
+       {/* <Grid container justify='space-between' alignItems='center' spacing={4}>
+                    <Grid item xs={8}>
+                        <Typography variant='h4'>
+                            Comments
+                        </Typography>
+                    </Grid>
+                </Grid> */}
+        
+      {props.data.map((comment) => (
+      <Paper variant='outlined' style={{padding: 10, marginTop: 20}}>
+      <Grid container justify='space-between' alignItems='flex-start' spacing={2}>
+          <Grid item>
+        
+              <Typography variant='h6'>
+              {comment.company_name_field}
+  </Typography>
+  
+              {/* <Typography variant='subtitle1' color="textSecondary">
+             fds
+  </Typography> */}
+          </Grid>
+          <Grid item>
+              <Typography variant='subtitle2'>
+                  {new Date(comment.created_on).toDateString()}
+  </Typography>
+          </Grid>
+          <Grid item xs={12}>
+          <Grid container justify="flex-start">
+              <Typography>
+              {comment.comment}
+      </Typography></Grid>
+          </Grid>
       </Grid>
+  </Paper>
+      
       ))}
-      </List>
+      
     </div>
   );
 }
