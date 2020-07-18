93858b7ff37a240d35d9bda06a5ed8f2c8b39f9f
manjunathvustepalli <manjunathvustepalli@gmail.com>
2020-07-18 12:14:27 +0530

    pulling from jeevan

--- src/Components/DashBoardComponents/MyCodes/EmployementCodes/Comments.js
+++ src/Components/DashBoardComponents/MyCodes/EmployementCodes/Comments.js
@@ -30,51 +30,50 @@ import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListItemText';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListItemText";
 
 function Comments(props) {
   return (
     <div>
-       {/* <Grid container justify='space-between' alignItems='center' spacing={4}>
+      {/* <Grid container justify='space-between' alignItems='center' spacing={4}>
                     <Grid item xs={8}>
                         <Typography variant='h4'>
                             Comments
                         </Typography>
                     </Grid>
                 </Grid> */}
-        
+
       {props.data.map((comment) => (
-      <Paper variant='outlined' style={{padding: 10, marginTop: 20}}>
-      <Grid container justify='space-between' alignItems='flex-start' spacing={2}>
-          <Grid item>
-        
-              <Typography variant='h6'>
-              {comment.company_name_field}
-  </Typography>
-  
+        <Paper variant="outlined" style={{ padding: 10, marginTop: 20 }}>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="flex-start"
+            spacing={2}
+          >
+            <Grid item>
+              <Typography variant="h6">{comment.company_name_field}</Typography>
+
               {/* <Typography variant='subtitle1' color="textSecondary">
              fds
   </Typography> */}
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle2">
+                {new Date(comment.created_on).toDateString()}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Grid container justify="flex-start">
+                <Typography>{comment.comment}</Typography>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item>
-              <Typography variant='subtitle2'>
-                  {new Date(comment.created_on).toDateString()}
-  </Typography>
-          </Grid>
-          <Grid item xs={12}>
-          <Grid container justify="flex-start">
-              <Typography>
-              {comment.comment}
-      </Typography></Grid>
-          </Grid>
-      </Grid>
-  </Paper>
-      
+        </Paper>
       ))}
-      
     </div>
   );
 }
