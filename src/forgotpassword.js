bee2d17061190ba2c301c9ad20b41a63449901a4
manjunathvustepalli <manjunathvustepalli@users.noreply.github.com>
2020-07-21 23:15:36 +0530

    check

--- src/forgotpassword.js
+++ src/forgotpassword.js
@@ -43,6 +43,7 @@ export default class forgotpassword extends Component {
         res.status === 200 ? this.setState({ sucess: true }) : ""
       )
       .catch((err) => {
+        console.log("err")
         this.setState({
           response: err.response.status,
           addsnackbar: true,
